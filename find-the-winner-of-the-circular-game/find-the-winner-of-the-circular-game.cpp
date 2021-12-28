46e1dc6cc3f5d983eff652aa1bb6e17c702a019a
RishiM12 <57403786+RishiM12@users.noreply.github.com>
2021-12-28 16:13:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.03%) - LeetHub

--- find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
+    int sol(int n,int k){
+         if (n==1) return 0;
+       else return (sol(n-1,k)+k)%n;
+    }
+    
     int findTheWinner(int n, int k) {
-        vector<int> v;
-        for (int i=1;i<n+1;i++){
-            v.push_back(i);
-        }
-        
-        
-        int pos=0;
-        while (v.size()!=1) {
-            pos=pos+k-1;
-            cout<<pos<<endl;
-            
-            while (pos>=v.size()){
-                pos=pos-v.size();
-                //v.erase(v.begin()+pos+1);
-            } 
-            v.erase(v.begin()+pos);
-        }
-        return v[0];
+      return sol(n,k)+1;
     }
 };
